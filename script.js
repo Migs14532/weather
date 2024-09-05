1714a9e319b91d3d8db12594c93589e0ea4bc1ae
Migs14532 <miguelpaolobarafon.202301454@gmail.com>
2024-09-05 15:10:57 +0800

    sad

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ document.addEventListener('DOMContentLoaded', async function() {
         weatherContainer.innerHTML += `
           
         <div class="weather-container">
-        <div class="weather-items">
+        <div class="weather-item">
             <img src="https:${element.day.condition.icon}">
         </div>
         <div class="weather-item">
